7bcd739dd07060eae2461d777928122fb6714104
Freddiew20 <deckerexomg@gmail.com>
2017-02-16 21:17:17 +0100

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,10 @@
+#-*- coding: utf-8 -*-
+class ProgramaPython:
+    algo = []
+    cad = input('Escribe un nombre: ')
+    print (cad, '--', len(cad))
+    print (cad[::-1])
+    #creando array de strings
+    algo=["hola","polo","pelo","aula","sol","omg"]
+    for var in algo:
+        print (var)
